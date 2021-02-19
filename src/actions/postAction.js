c931fe3b42410a6c47d1318729bf2e611f0d057b
Samuel Opoku Asare <asare11samuel@gmail.com>
2021-02-19 03:23:31 +0000

    Recreated postAction.js in actions

--- src/actions/postAction.js
+++ src/actions/postAction.js
@@ -2,6 +2,17 @@ import {FETCH_POSTS, NEW_POST} from './types';
 
 export function fetchPosts(){
 
-    
+    return function(dispatch){
+
+        fetch("https://jsonplaceholder.typicode.com/posts")
+        .then(res => res.json())
+        .then(data => dispatch({
+
+            type: FETCH_POSTS,
+            payload: data
+
+        }))
+
+    }
 
 }
